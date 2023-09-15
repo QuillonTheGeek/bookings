c1c5d433131eebb714d95b03ffb885caef03a7a9
Quillon <quillonthegeek@gmail.com>
2023-09-15 21:00:10 +0100

    connect Hotel FE to BE

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -11,28 +11,30 @@ import {
 import MailList from "../../components/mailList/MailList";
 import Footer from "../../components/footer/Footer";
 import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import { useLocation } from "react-router-dom";
 
 const Hotel = () => {
-  const photos = [
-    {
-      src: "https://images.pexels.com/photos/3952034/pexels-photo-3952034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      src: "https://images.pexels.com/photos/2343465/pexels-photo-2343465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      src: "https://images.pexels.com/photos/5825527/pexels-photo-5825527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      src: "https://images.pexels.com/photos/6021777/pexels-photo-6021777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      src: "https://images.pexels.com/photos/4906249/pexels-photo-4906249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      src: "https://images.pexels.com/photos/4825713/pexels-photo-4825713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-  ];
+  // const photos = [
+  //   {
+  //     src: "https://images.pexels.com/photos/3952034/pexels-photo-3952034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     src: "https://images.pexels.com/photos/2343465/pexels-photo-2343465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     src: "https://images.pexels.com/photos/5825527/pexels-photo-5825527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     src: "https://images.pexels.com/photos/6021777/pexels-photo-6021777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     src: "https://images.pexels.com/photos/4906249/pexels-photo-4906249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  //   {
+  //     src: "https://images.pexels.com/photos/4825713/pexels-photo-4825713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //   },
+  // ];
 
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
@@ -51,90 +53,110 @@ const Hotel = () => {
     }
     setSlideNumber(newSlideNumber);
   };
+  const location = useLocation();
+  // console.log(location);
+  const id = location.pathname.split("/")[2];
+  const { data, loading, error, reFetch } = useFetch(`/api/hotels/find/${id}`);
+
   return (
     <div>
       <Navbar />
       <Header type="list" />
-      <div className="hotelContainer">
-        {open && (
-          <div className="slider">
-            <FontAwesomeIcon
-              icon={faCircleXmark}
-              className="close"
-              onClick={() => setOpen(false)}
-            />
-            <FontAwesomeIcon
-              icon={faCircleArrowLeft}
-              className="arrow"
-              onClick={() => handleMove("l")}
-            />
-            <div className="sliderWrapper">
-              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
-            </div>
-            <FontAwesomeIcon
-              icon={faCircleArrowRight}
-              className="arrow"
-              onClick={() => handleMove("r")}
-            />
-          </div>
-        )}
-        <div className="hotelWrapper">
-          <button className="bookNow">Reserve or Book Now</button>
-          <h1 className="hotelTitle">Grand Hotel</h1>
-          <div className="hotelAAddress">
-            <FontAwesomeIcon icon={faLocationDot} />
-            <span>Elton St 125 New york</span>
-          </div>
-          <span className="hotelDistance">
-            Excellent location - 500m from center
-          </span>
-          <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
-          </span>
-          <div className="hotelImages">
-            {photos.map((photo, index) => (
-              <div className="hotelImgWrapper" key={index}>
+      {loading ? (
+        "Getting details..."
+      ) : (
+        <div className="hotelContainer">
+          {open && (
+            <div className="slider">
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                className="close"
+                onClick={() => setOpen(false)}
+              />
+              <FontAwesomeIcon
+                icon={faCircleArrowLeft}
+                className="arrow"
+                onClick={() => handleMove("l")}
+              />
+              <div className="sliderWrapper">
+                {/* <img src={photos[slideNumber].src} alt="" className="sliderImg" /> */}
                 <img
-                  onClick={() => handleOpen(index)}
-                  src={photo.src}
-                  className="hotelImg"
+                  src={data.photo[slideNumber]}
+                  alt=""
+                  className="sliderImg"
                 />
               </div>
-            ))}
-          </div>
-          <div className="hotelDetails">
-            <div className="hotelDetailTexts">
-              <h1 className="hotelTitle">
-                🌟 Luxurious 2-Bedroom Apartment Getaway 🌟
-              </h1>
-              <p className="hotelDesc">
-                Escape to the epitome of comfort and elegance at our exquisite
-                hotel's 2-bedroom apartment. Discover a world of indulgence and
-                relaxation as you step into your home away from home. 🏡 Your
-                Sanctuary Away from Home 🏡 Nestled in the heart of [City Name],
-                our spacious 2-bedroom apartment offers you the perfect blend of
-                sophistication and homely warmth. With meticulously designed
-                interiors, you'll find every detail thoughtfully curated to
-                ensure your utmost comfort and convenience. ✨ Apartment
-                Highlights ✨
-              </p>
+              <FontAwesomeIcon
+                icon={faCircleArrowRight}
+                className="arrow"
+                onClick={() => handleMove("r")}
+              />
             </div>
-            <div className="hotelDetailPrice">
-              <h1>Perfect for a 8-night stay!</h1>
+          )}
+          <div className="hotelWrapper">
+            <button className="bookNow">Reserve or Book Now</button>
+            <h1 className="hotelTitle">
+              {/* Grand Hotel */}
+              {data.name}
+            </h1>
+            <div className="hotelAAddress">
+              <FontAwesomeIcon icon={faLocationDot} />
               <span>
-                Located in Magodo, Ikosi.Lagos , this resort has a 9.1 excellent
-                location score
+                {/* Elton St 125 New york */}
+                {data.address}
               </span>
-              <h2>
-                <b>$1254.97</b>(9 nights)
-              </h2>
-              <button>Reserve or Book now</button>
+            </div>
+            <span className="hotelDistance">
+              Excellent location - {data.distance}m from center
+            </span>
+            <span className="hotelPriceHighlight">
+              Book a stay over ${data.cheapestPrice} at this property and get a
+              free airport taxi
+            </span>
+            <div className="hotelImages">
+              {data.photos?.map((photo, index) => (
+                <div className="hotelImgWrapper" key={index}>
+                  <img
+                    onClick={() => handleOpen(index)}
+                    // src={photo.src}
+                    src={data.photo}
+                    className="hotelImg"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="hotelDetails">
+              <div className="hotelDetailTexts">
+                <h1 className="hotelTitle">🌟{data.desc} 🌟</h1>
+                <p className="hotelDesc">
+                  Escape to the epitome of comfort and elegance at our exquisite
+                  hotel's 2-bedroom apartment. Discover a world of indulgence
+                  and relaxation as you step into your home away from home. 🏡
+                  Your Sanctuary Away from Home 🏡 Nestled in the heart of [City
+                  Name], our spacious 2-bedroom apartment offers you the perfect
+                  blend of sophistication and homely warmth. With meticulously
+                  designed interiors, you'll find every detail thoughtfully
+                  curated to ensure your utmost comfort and convenience. ✨
+                  Apartment Highlights ✨
+                </p>
+              </div>
+              <div className="hotelDetailPrice">
+                <h1>Perfect for a 8-night stay!</h1>
+                <span>
+                  Located in Magodo, Ikosi.Lagos , this resort has a 9.1
+                  excellent location score
+                </span>
+                <h2>
+                  <b>$1254.97</b>(9 nights)
+                </h2>
+                <button>Reserve or Book now</button>
+              </div>
             </div>
           </div>
+          <MailList />
+          <Footer />
         </div>
-        <MailList />
-        <Footer />
-      </div>
+      )}
     </div>
   );
 };
