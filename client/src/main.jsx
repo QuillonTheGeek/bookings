5da25c7dcc33bc7a0b1fab4bbde84aa28e704630
Quillon <quillonthegeek@gmail.com>
2023-09-15 21:00:38 +0100

    SearchConrtext Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { SearchContextProvider } from "./context/SearchContext.js";
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
   </React.StrictMode>
 );
