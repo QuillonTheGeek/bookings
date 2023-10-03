73cd4f35ee5cc94090c73a1104e4366b1d1b2cf7
Quillon <quillonthegeek@gmail.com>
2023-10-03 21:07:11 +0100

    charts

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./Home.scss";
 import Sidebar from "./../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
@@ -15,7 +17,11 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
-        </div> 
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   );
