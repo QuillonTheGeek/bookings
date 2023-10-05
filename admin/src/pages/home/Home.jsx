1374f8a29ce6e49b796397a74942907fa9eff52a
Quillon <quillonthegeek@gmail.com>
2023-10-05 18:20:26 +0100

    data table

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 
 const Home = () => {
   return (
@@ -11,7 +12,6 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-
         <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
@@ -21,6 +21,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
